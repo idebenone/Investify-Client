8ce4ef6ad961a980959b893c3469f961d08a5df2
Vineeth <vineeth.v.gooli@gmail.com>
2023-05-15 17:58:22 +0530

    pull check

--- src/app/Components/cmp-pitchess/cmp-pitchess.component.ts
+++ src/app/Components/cmp-pitchess/cmp-pitchess.component.ts
@@ -16,6 +16,7 @@ export class CmpPitchessComponent {
     pitch_id: new FormControl(''),
     cmp_id: new FormControl(''),
     pitch_title: new FormControl('', [Validators.required]),
+    pitch_desc: new FormControl('', [Validators.required]),
     feature_img: new FormControl(''),
     feature_vid: new FormControl('', [Validators.required]),
     deck: new FormControl('', [Validators.required])
@@ -62,12 +63,48 @@ export class CmpPitchessComponent {
     const newPitchObj = {
       cmp_id: this.userID,
       pitch_title: this.pitchObj['pitch_title'].value,
+      pitch_desc: this.pitchObj['pitch_desc'].value,
       feature_vid: this.pitchObj['feature_vid'].value,
       deck: this.pitchObj['deck'].value
     }
 
     this.pitchService.createPitch(newPitchObj).subscribe((data) => {
       this.snack.open('Created Successfully', 'OK', { duration: 4000 });
+      this.ngOnInit();
+    })
+  }
+
+  getPitch(id: any) {
+    this.pitchService.getPitchById(id).subscribe((data: any) => {
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+          const value = data[key];
+          this.pitchObj[key].setValue(value);
+        }
+      }
+    })
+  }
+
+  updatePitch() {
+    const updatePitchObj = {
+      pitch_id: this.pitchObj['pitch_id'].value,
+      cmp_id: this.userID,
+      pitch_title: this.pitchObj['pitch_title'].value,
+      pitch_desc: this.pitchObj['pitch_desc'].value,
+      feature_vid: this.pitchObj['feature_vid'].value,
+      deck: this.pitchObj['deck'].value
+    }
+
+    this.pitchService.updatePitch(updatePitchObj).subscribe((data: any) => {
+      this.snack.open('Updated Successfully', 'OK', { duration: 4000 });
+      this.ngOnInit();
+    })
+
+  }
+
+  deletePitch(id: any) {
+    this.pitchService.deletePitch(id).subscribe((data: any) => {
+      this.snack.open("Deleted Successfully", "OK", { duration: 4000 });
       this.ngOnInit();
     })
   }
